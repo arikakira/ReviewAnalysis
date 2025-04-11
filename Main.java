1261cbb6122a949f6a13f642065a4070029f08c1
arikakira <samanthal242@nycstudents.net>
2025-04-11 01:52:24 +0000

    Update

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Review[] reviews = {
+            new Review(4, "Good, Thx!"),
+            new Review(3, "OK site"),
+            new Review(5, "Great!"),
+            new Review(2, "Poor! Bad."),
+            new Review(3, "")
+        };
+        ReviewAnalysis r = new ReviewAnalysis(reviews);
+        System.out.println(r.getAverageRating());       // test case 1
 
+        System.out.println(r.collectComments());        // test csae 2
+    }
+}
--- ReviewAnalysis.java
+++ ReviewAnalysis.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
+
 public class ReviewAnalysis {
     /** All user reviews to be included in this analysis */
     private Review[] allReviews;
 
 
     /** Initializes allReviews to contain all the Review objects to be analyzed */
-    public ReviewAnalysis() {
-        /* implementation not shown */ }
+    public ReviewAnalysis(Review[] reviews) {
+        allReviews = reviews;
+    }
 
 
     /**
@@ -16,7 +19,12 @@ public class ReviewAnalysis {
      * No element of allReviews is null.
      */
     public double getAverageRating() {
-        /* to be implemented in part (a) */ }
+        double sum = 0;
+        for(Review r : allReviews) {
+            sum += r.getRating();
+        }
+        return sum/allReviews.length;
+    }
 
 
     /**
@@ -27,5 +35,17 @@ public class ReviewAnalysis {
      * Postcondition: allReviews is unchanged.
      */
     public ArrayList<String> collectComments() {
-        /* to be implemented in part (b) */ }
+        ArrayList<String> comments = new ArrayList<String>();
+        for(int i=0; i<allReviews.length; i++) {
+            String s = i + "-" + allReviews[i].getComment();
+            String last = s.substring(s.length()-1);
+            if(!last.equals(".") && !last.equals("!")) {
+                s += ".";
+            }
+            if(s.indexOf("!") > 0) {
+                comments.add(s);
+            }
+        }
+        return comments;
+    }
 }
